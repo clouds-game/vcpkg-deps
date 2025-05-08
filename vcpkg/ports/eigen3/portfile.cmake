3d5f8094a140d66ffea0bbe749ea2fd378a08979
Clouds Flowing <clouds.flowing@gmail.com>
2025-05-08 23:59:09 +0800

    fix onnxruntime

--- vcpkg/ports/eigen3/portfile.cmake
+++ vcpkg/ports/eigen3/portfile.cmake
@@ -1,20 +1,17 @@
-vcpkg_buildpath_length_warning(37)
-
-block(SCOPE_FOR VARIABLES PROPAGATE SOURCE_PATH)
-set(VCPKG_BUILD_TYPE release) # header-only
+set(EIGEN3_GIT_COMMIT "68f4e58cfacc686583d16cff90361f0b43bc2c1b")
 
 vcpkg_from_gitlab(
     GITLAB_URL https://gitlab.com
     OUT_SOURCE_PATH SOURCE_PATH
     REPO libeigen/eigen
-    REF "${VERSION}"
-    SHA512 ba75ecb760e32acf4ceaf27115468e65d4f77c44f8d519b5a13e7940af2c03a304ad433368cb6d55431f307c5c39e2666ab41d34442db3cf441638e51f5c3b6a
+    REF "${EIGEN3_GIT_COMMIT}"
+    SHA512 5b1c1bc02b78128c3b83316a38e1cbc413b16be8ea59e7fc4f3faaf433f9b00d2c0e3abfb36aed24c48c5eddf184fb3ad5c29f615243dd9ab0ce6e0408b8d23a
     HEAD_REF master
-    PATCHES
-        remove_configure_checks.patch # This removes unnecessary configure checks. Eigen3 just installs headers not anything more.
-        fix-vectorized-reductions-half.patch # Remove this patch in the next update
-        update-warning-suppression-to-latest.patch
 )
+
+if(VCPKG_TARGET_IS_ANDROID OR VCPKG_TARGET_IS_IOS)
+    list(APPEND PLATFORM_OPTIONS -DCMAKE_Fortran_COMPILER=OFF)
+endif()
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
@@ -22,22 +19,23 @@ vcpkg_cmake_configure(
         -DBUILD_TESTING=OFF
         -DEIGEN_BUILD_DOC=OFF
         -DEIGEN_BUILD_PKGCONFIG=ON
-        "-DCMAKEPACKAGE_INSTALL_DIR=${CURRENT_PACKAGES_DIR}/share/eigen3"
-        "-DPKGCONFIG_INSTALL_DIR=${CURRENT_PACKAGES_DIR}/lib/pkgconfig"
+        ${PLATFORM_OPTIONS}
+    OPTIONS_RELEASE
+        -DCMAKEPACKAGE_INSTALL_DIR=${CURRENT_PACKAGES_DIR}/lib/cmake/${PORT}
+        -DPKGCONFIG_INSTALL_DIR=${CURRENT_PACKAGES_DIR}/lib/pkgconfig
+    OPTIONS_DEBUG
+        -DCMAKEPACKAGE_INSTALL_DIR=${CURRENT_PACKAGES_DIR}/debug/lib/cmake/${PORT}
+        -DPKGCONFIG_INSTALL_DIR=${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig
 )
 
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup()
-vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/share/eigen3/Eigen3Config.cmake" "if (NOT TARGET eigen)" "if (NOT TARGET Eigen3::Eigen)")
-endblock()
-
-if(NOT VCPKG_BUILD_TYPE)
-    file(INSTALL "${CURRENT_PACKAGES_DIR}/lib/pkgconfig/eigen3.pc" DESTINATION "${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig")
-endif()
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/${PORT} PACKAGE_NAME Eigen3)
 vcpkg_fixup_pkgconfig()
 
-file(GLOB INCLUDES "${CURRENT_PACKAGES_DIR}/include/eigen3/*")
-# Copy the eigen header files to conventional location for user-wide MSBuild integration
-file(COPY ${INCLUDES} DESTINATION "${CURRENT_PACKAGES_DIR}/include")
+file(REMOVE_RECURSE
+    "${CURRENT_PACKAGES_DIR}/debug/include"
+    "${CURRENT_PACKAGES_DIR}/debug/share"
+)
 
+file(INSTALL "${SOURCE_PATH}/COPYING.README" DESTINATION "${CURRENT_PACKAGES_DIR}/share")
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/COPYING.README")
